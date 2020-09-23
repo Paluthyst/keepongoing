84dcaa1f0bd80accce26bdf94c0fa5f526e099c7
Paluthyst <66975756+Paluthyst@users.noreply.github.com>
2020-09-23 19:30:19 -0400

    Add project files.

---
+++ keepongoing/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace keepongoing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool pumpItUp = true;
+            int numberOfPumps = 0;
+            
+
+            while(pumpItUp){
+
+                Console.WriteLine("You've got to pump it up! don't you know pump it up?");
+                numberOfPumps ++;
+                Console.Write("Wanna keep pumping it up? Yes or No?");
+                pumpItUp = getInput();
+                
+            }
+        }
+        public static bool getInput()
+        {
+            string keepPumping = Console.ReadLine();
+            bool pumpItUp = true;
+
+            if(keepPumping == "No" || keepPumping == "no" || keepPumping == "n" || keepPumping == "N")
+            {
+                Console.WriteLine("You've stopped pumping it up");
+                
+                pumpItUp = false;
+            }
+
+            return pumpItUp;
+        }
+    }
+}
